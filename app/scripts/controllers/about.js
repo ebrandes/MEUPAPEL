b485352cde6b72093f537f71e95677cdcb407c4a
ebrandes <eduardocbrandes@gmail.com>
2015-05-29 00:15:21 -0300

    INITIAL COMMIT
    primeiro commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name meupapelApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the meupapelApp
+ */
+angular.module('meupapelApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name meupapelApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the meupapelApp
+ */
+angular.module('meupapelApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
